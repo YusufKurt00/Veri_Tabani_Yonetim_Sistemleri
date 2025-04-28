2cfda25fb3b8ebf31cf62d548fd934bb0507ed9f
Yusuf Kurt <Yusuf_kurt23@hotmail.com>
2025-04-28 21:51:00 +0300

    Update influxdbye_yaz.py

--- Döviz/influxdbye_yaz.py
+++ Döviz/influxdbye_yaz.py
@@ -1,14 +1,17 @@
 from influxdb_client import InfluxDBClient, Point,WritePrecision
 from datetime import datetime,timezone
 import requests
+import time
 
 API_KEY = "63cc8fb0c37a3c006bf3eb5c"
 INFLUXDB_URL = "http://localhost:8086"
 INFLUXDB_TOKEN = "3LtuNGSVl8XuGrmHEyAvC9bZIDDWkYE79SyUj5T4gRuc40NLmLFd8oqL0ZHAR_j-WmmVOAVFWSMyIP3Du9jgmA=="
 INFLUXDB_ORG = "Deneme"
-INFLUXDB_BUCKET = "doviz"
+INFLUXDB_BUCKET = "Deneme"
 
 BASE_URL = f"https://v6.exchangerate-api.com/v6/{API_KEY}/latest/USD"
+
+DOVIZ_LISTESI = ["USD", "EUR", "GBP", "JPY", "CHF", "CAD", "AUD", "CNY", "RUB", "SAR", "NOK", "SEK", "DKK", "AED"]
 
 def doviz_kuru_al():
     
@@ -24,21 +27,31 @@ def influxdbye_yaz(rates):
     client = InfluxDBClient(url=INFLUXDB_URL, token=INFLUXDB_TOKEN, org=INFLUXDB_ORG)
     write_api = client.write_api()
     
+    usd_to_try = rates['TRY']  
+    
     point = Point("doviz_kuru")\
-        .tag("base", "USD")\
-        .field("EUR", rates['EUR']) \
-        .field("GBP", rates['GBP']) \
-        .field("JPY", rates['JPY']) \
-        .field("TRY", rates['TRY']) \
+        .tag("base", "TRY")\
         .time(datetime.now(timezone.utc), WritePrecision.NS)
-        
-    write_api.write(bucket=INFLUXDB_BUCKET,org=INFLUXDB_ORG, record=point)
-    print("Veri InfluxDB'ye yazıldı.")
-    write_api.close()
-    client.close()
+
+    point = point.field("TRY", 1.0)
+
+    for doviz in DOVIZ_LISTESI:
+        if doviz in rates:
+            value = usd_to_try / rates[doviz]
+            point = point.field(doviz, round(value, 4))  
+    
+    write_api.write(bucket=INFLUXDB_BUCKET, org=INFLUXDB_ORG, record=point)
+    print("Veri yazıldı:", datetime.now())
     
 if __name__ == "__main__":
-    doviz = doviz_kuru_al()
-    influxdbye_yaz(doviz)
+    while True:
+        try:
+            doviz = doviz_kuru_al()
+            print(doviz)
+            influxdbye_yaz(doviz)
+        except Exception as e:
+            print("Hata:", e)
+        
+        time.sleep(9)
     
 
